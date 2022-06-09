6be8e81b94bdf59a32be3a4d548b47c4be7a574e
helenren <qren@salesforce.com>
2022-06-08 21:56:28 -0700

    address more comments

--- wdio.conf.ios.js
+++ wdio.conf.ios.js
@@ -1,39 +1,7 @@
-require('dotenv').config();
-
-const path = require('path');
-const { UtamWdioService } = require('wdio-utam-service');
-const { DEBUG } = process.env;
-const EXPLICIT_TIMEOUT = 60 * 1000;
-const DEBUG_TIMEOUT = EXPLICIT_TIMEOUT * 30;
+const { baseMobileConfig } = require('./wdio.conf.shared');
 
 exports.config = {
-    //
-    // ====================
-    // Runner Configuration
-    // ====================
-    //
-    // WebdriverIO allows it to run your tests in arbitrary locations (e.g. locally or
-    // on a remote machine).
-    runner: 'local',
-    port: 4444,
-
-    //
-    // ==================
-    // Specify Test Files
-    // ==================
-    // Define which test specs should run. The pattern is relative to the directory
-    // from which `wdio` was called.
-    specs: ['force-app/test/mobile/**/*.spec.js'],
-    //
-    // ============
-    // Capabilities
-    // ============
-    // Define your capabilities here. WebdriverIO can run multiple capabilities at the same
-    // time. Depending on the number of capabilities, WebdriverIO launches several test
-    // sessions. Within your capabilities you can overwrite the spec and exclude options in
-    // order to group specific specs to a specific capability.
-
-    maxInstances: 1,
+    ...baseMobileConfig,
     capabilities: [
         {
             platformName: 'iOS',
@@ -44,20 +12,4 @@ exports.config = {
             'appium:platformVersion': '15.2',
         },
     ],
-    logLevel: 'info',
-    bail: 0,
-    baseUrl: 'http://localhost',
-    // timeout for all waitFor commands
-    waitforTimeout: DEBUG ? DEBUG_TIMEOUT : EXPLICIT_TIMEOUT,
-    connectionRetryTimeout: 120000,
-    connectionRetryCount: 3,
-    services: ['selenium-standalone', [UtamWdioService, { implicitTimeout: 0 }]],
-    framework: 'jasmine',
-    reporters: ['spec'],
-    jasmineNodeOpts: {
-        // max execution time for a script, set to 5 min
-        defaultTimeoutInterval: 1000 * 60 * 5,
-        // Temporary workaround to get babel to work in wdio tests
-        helpers: [path.resolve(process.cwd(), 'wdioJasmineHelper.js')],
-    },
 };
